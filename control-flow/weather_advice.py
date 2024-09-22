2cb2d5339b66f8a81fd164682547dcbe3d9f16cd
Kwabena <79258511+kobby18-50@users.noreply.github.com>
2024-09-22 06:34:30 +0000

    updated quotes2

--- control-flow/weather_advice.py
+++ control-flow/weather_advice.py
@@ -1,4 +1,4 @@
-response = input("What's the weather like today? (sunny/rainy/cold)\n")
+response = input("What's the weather like today? (sunny/rainy/cold):")
 
 if(response == 'sunny'):
     print('Wear a t-shirt and sunglasses')
