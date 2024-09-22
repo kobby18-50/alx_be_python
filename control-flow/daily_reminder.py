f88c586f804634d875c403f8dace99a770292660
Kwabena <79258511+kobby18-50@users.noreply.github.com>
2024-09-22 07:55:00 +0000

    daily reminder

---
+++ control-flow/daily_reminder.py
@@ -0,0 +1,20 @@
+task = input("Enter your task: ")
+
+priority = input("Priority (high/medium/low)")
+
+time_bound = input("Is it time-bound? (yes/no)")
+
+match priority:
+    case 'high':
+        if(time_bound == 'yes'):
+            print(f"{task} is high priority task that requires immediate attention today!")
+    case 'medium':
+        if(time_bound == 'yes'):
+            print(f"{task} is a medium priority task that requires immediate attention today!")
+    case 'low':
+        if(time_bound == 'no'):
+            print(f"{task} is a low priority task. Consider completing it when you have free time")
+    case _:
+        print('Work at your own pace')
+
+    
